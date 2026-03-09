40b4511914a4a6bdd8b5b68c468cb36d6ae50477
Anuj Dubey <anujdubey.ok@gmail.com>
2026-03-10 03:59:07 +0530

    fleat: created middleware from multer , added cloudnary for uploading data

---
+++ src/utils/cloudnary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+ const uploadToCloudinary = async (filePath) => {
+    try {
+        if(!filePath)return null
+            // upload file to cloudinary
+       
+       const response = await  cloudinary.uploader.upload
+       (filePath, { 
+            resource_type: "auto"
+        })
+        console.log('Cloudinary upload response:', response.url);
+        return response;    
+
+    } catch (error) {
+       fs.unlinkSync(filePath) // delete the file from local storage
+       console.error('Cloudinary upload error:', error);
+       return null;
+    }
+}
+
+export default uploadToCloudinary;
